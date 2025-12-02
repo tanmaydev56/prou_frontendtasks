abe23aa350c975a11e1dda3812bf3005ce3ad4e9
Tanmay Sharma <tanmaysharma763@gmail.com>
2025-12-02 13:49:18 +0530

    wdad

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -61,7 +61,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           
-          {/* Branding */}
+         
           <div className="flex items-center space-x-3">
             <div className="w-8 h-8 bg-linear-to-br from-blue-600 to-blue-800 rounded-lg flex items-center justify-center">
               <span className="text-white font-bold text-sm">TT</span>
