ad4136e47dda7aa6deb5a595bdb9e3a3182c260b
Tanmay Sharma <tanmaysharma763@gmail.com>
2025-12-02 13:48:49 +0530

    Wda

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ export const API = axios.create({
   baseURL:"https://backend-prou.onrender.com"
 });
 
-// Attach role and user info to every request
+
 API.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem("currentUser"));
   if (user) config.headers["x-user-role"] = user.role;
